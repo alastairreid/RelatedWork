e0aeb7907ae3a519c08ee0adae0b028f1ab25f86
Alastair Reid <alastair.d.reid@gmail.com>
2020-07-12 11:49:34 +0100

    New script for creating blank notes

---
+++ _scripts/note.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import json
+import yaml
+from yaml import dump
+from yaml import CDumper as Dumper
+
+from datetime import date
+
+import re
+import string
+import sys
+
+def main():
+    name = sys.argv[1]
+    title = " ".join(sys.argv[2:])
+    entry = {
+        "title": title,
+        "layout": "note",
+        "isa": {},
+        "examples": {},
+        "notes": {},
+        "papers": {}
+        }
+    with open(f"_notes/{name}.md", 'w') as f:
+        yaml.dump(entry, Dumper=Dumper, explicit_start=True, allow_unicode=True, width=150, stream=f)
+        print("---", file=f)
+
+main()
